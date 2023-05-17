793cc3cec4898941b56cdc370c03656e67d86737
pmemoli <pedromemoli@hotmail.com>
2023-05-16 21:32:47 -0300

    fixeado algo

--- src/datos/utilidades/tiemposCache.ts
+++ src/datos/utilidades/tiemposCache.ts
@@ -4,9 +4,9 @@ interface tiemposCache {
 
 // Tiempo en ms
 const msEnMinuto: number = 60 * 1000;
-const msEnHora: number = 1000 * 60 * 60;
+const msEnHora: number = msEnMinuto * 60;
 const msEnDia: number = msEnHora * 24;
-const msEnMes: number = msEnHora * 24 * 30;
+const msEnMes: number = msEnDia * 30;
 
 export const tiemposCache: tiemposCache = {
     "dolar": msEnMinuto * 10,
@@ -15,8 +15,8 @@ export const tiemposCache: tiemposCache = {
     "empleo": msEnDia * 1,
     "gasto": msEnDia * 1,
     "inflacion": msEnDia * 1,
-    "merval": msEnHora * 0.5,
-    "riesgo": msEnHora * 0.5,
+    "merval": msEnMinuto * 10,
+    "riesgo": msEnMinuto * 10,
     "pobreza": msEnMes * 3,
     "ingresos": msEnDia * 1,
     "distribucion": msEnDia * 1
