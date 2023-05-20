0192c19dbef9b3822fe05d3b51b3846b5de608da
pmemoli <pedromemoli@hotmail.com>
2023-05-20 12:57:11 -0300

    soy un down

--- src/datos/utilidades/getDataAnalitica.ts
+++ src/datos/utilidades/getDataAnalitica.ts
@@ -67,8 +67,6 @@ export async function getDataAnalitica(nombre, cacheData, setCacheData, setData,
 
       let datosApi: any = res.data.datos;
 
-      console.log(datosApi);
-
       delete datosApi['nombre'];
       delete datosApi['__v'];
 
