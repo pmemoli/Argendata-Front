2b78e9778c3744d0a6850504794fe52304d7841f
pmemoli <pedromemoli@hotmail.com>
2023-08-13 19:52:47 -0300

    arreglado los drodowns que en mobile cagaban el css

--- src/datos/gui/components/DropdownMenu.jsx
+++ src/datos/gui/components/DropdownMenu.jsx
@@ -14,7 +14,7 @@ export default function DropdownMenu({optionArray, selectedOption, setOption}) {
 
   return (
     <div className='mt-1 sm:mt-0'>
-      <div className={`absolute w-16 sm:w-40 ml-1 sm:text-xl sm:p-1 truncate whitespace-nowrap `}>
+      <div className={`absolute w-24 sm:w-40 ml-1 sm:text-xl sm:p-1 truncate whitespace-nowrap `}>
         <select defaultValue={selectedOption} onChange={(e) => setOption(e.target.value)} className='sm:h-10 font-fira rounded-md text-white bg-transparent h-7 '>
           {optionArray.map(option => (
             <option className='text-black truncate'>
