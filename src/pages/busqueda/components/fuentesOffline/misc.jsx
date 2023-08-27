3c75237cd62f4b96fe7afcedce80c5d39e82e719
pmemoli <pedromemoli@hotmail.com>
2023-08-27 15:06:04 -0300

    agregado deficit

--- src/pages/busqueda/components/fuentesOffline/misc.jsx
+++ src/pages/busqueda/components/fuentesOffline/misc.jsx
@@ -5,5 +5,10 @@ export const datosMisc = [
         descripcion: 'La deuda bruta corresponde al total de las obligaciones contractuales públicas, privadas y con organismos internacionales que asume el Estado para financiarse. Se presentan los gráficos elaborados en base a los últimos datos disponibles, que marcan la evolución y composición del total de la deuda.',
         path: 'https://www.argentina.gob.ar/economia/finanzas/graficos-deuda'
     },
+    {
+        nombre: 'Estadísticas de la cancilleria',
+        fuente: 'cancilleria.gob.ar',
+        descripcion: 'Estadísticas de actividad economica, finanzas públicas (resultado fiscal, reservas, deuda), precios y comercio exterior.',
+        path: 'https://cancilleria.gob.ar/es/cei/estadisticas'
+    },
 ]
-// https://www.argentina.gob.ar/economia/finanzas/graficos-deuda
