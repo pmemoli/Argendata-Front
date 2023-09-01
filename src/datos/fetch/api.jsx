74850d49b234459321652b8a745fe58e9b0d7e23
pmemoli <pedromemoli@hotmail.com>
2023-09-01 00:23:16 -0300

    cambio de api

--- src/datos/fetch/api.jsx
+++ src/datos/fetch/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const modo = 'test'
+const modo = 'production'
 const apiPath = modo === 'production' ? 'https://argendata-back.vercel.app' : 'http://localhost:5000'
 
 export const api = axios.create({
