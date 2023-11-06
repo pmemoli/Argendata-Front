71c83916341b2e792f9742638acaee178e6f8de1
pmemoli <pedromemoli@hotmail.com>
2023-11-06 00:17:29 -0300

    cambiado cositas de styling n shit

--- src/layout/header/components/SidebarItem.jsx
+++ src/layout/header/components/SidebarItem.jsx
@@ -9,14 +9,14 @@ export default function SidebarItem({nombre, datosDisponibles, setHeaderToggle})
   }
 
   return (
-    <div className='text-xl sm:text-xl mb-2'>
+    <div className='text-xl sm:text-2xl mb-2'>
       <div className='flex items-center mb-1'>
         <button onClick={toggleDropdown}>{nombre}</button>
         <img className='w-2 h-[0.3rem] mt-[0.1rem] sm:mt-0 ml-1 mr-2 sm:ml-2 sm:mr-0' src={require('../../../assets/dropdownIcon.png')} />
       </div>
 
       {isOpen && (
-        <ul className='ml-2 text-lg'>
+        <ul className='ml-2 text-lg sm:text-xl'>
           {datosDisponibles.map(dato => (
             <li onClick={() => {setIsOpen(false); setHeaderToggle(false)}}>
               <Link to={`/${dato.path}`}>{dato.nombre}</Link>
